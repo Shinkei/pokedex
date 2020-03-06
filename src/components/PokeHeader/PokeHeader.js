3282d5c7db554768eb2e8b76dcd0e798a98a32fe
Luv Karakoti <l.karakoti@sensory-minds.com>
2020-03-06 18:08:38 +0100

    Updates for aligning the header

--- src/components/PokeHeader/PokeHeader.js
+++ src/components/PokeHeader/PokeHeader.js
@@ -1,32 +1,32 @@
-import React from 'react'
+import React from "react";
 
 class PokeHeader extends React.Component {
-  constructor () {
-    super()
-    this.state = { dialog: '' }
+  constructor() {
+    super();
+    this.state = { dialog: "" };
   }
 
-  async componentDidMount () {
+  async componentDidMount() {
     this.setState({
-      dialog: 'Pokémon List'
-    })
+      dialog: "Pokémon List"
+    });
   }
 
-  render () {
-    const { dialog } = this.state
+  render() {
+    const { dialog } = this.state;
     return (
-      <header className='nes-container is-dark is-centered'>
-        <section className='message-list'>
-          <section className='message -left'>
-            <i className='nes-pokeball' />
-            <div className='nes-balloon from-left is-dark'>
+      <header className="nes-container is-dark is-centered">
+        <section className="message-list">
+          <section className="message -left">
+            <i className="nes-pokeball" />
+            <div className="nes-balloon from-left is-dark">
               <p>{dialog}</p>
             </div>
           </section>
         </section>
       </header>
-    )
+    );
   }
 }
 
-export default PokeHeader
+export default PokeHeader;

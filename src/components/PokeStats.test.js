1ac4c01c1045f7f16ba5862d5d5ba1bb7a6dfb0a
Luv Karakoti <l.karakoti@sensory-minds.com>
2020-03-06 17:08:35 +0100

    Snapshot updates

--- src/components/PokeStats.test.js
+++ src/components/PokeStats.test.js
@@ -5,23 +5,15 @@ import PokeStats from "./PokeStats";
 jest.mock("../services/pokedex.js");
 
 describe("PokeStats", () => {
-  it("Should render stats for Bulbasaur properly", () => {
+  it("Should render Bulbasaur component", async () => {
     const pokestats = render(<PokeStats pokemon="bulbasaur" />);
+    const { waitForNextUpdate, getByText } = pokestats;
+
+    // Necessary for the component to render after fetching API
+    await waitForNextUpdate;
+
     expect(pokestats).toMatchSnapshot();
-    const { container } = pokestats;
-
-    const table = container.querySelector(".nes-table");
-    const tr = table.querySelectorAll("tr");
-    const td = table.querySelectorAll("td");
-
-    expect(container).toContainElement(table);
-    expect(tr.length).toBe(5);
-    expect(td.length).toBe(11);
-
-    expect(td[0]).toHaveTextContent("Name");
-    expect(td[1]).toHaveTextContent("bulbasaur");
-
-    expect(td[3]).toHaveTextContent("Abilities");
-    expect(td[4]).toHaveTextContent(/chlorophyll/);
+    const nameElement = getByText("bulbasaur");
+    expect(nameElement).toBeInTheDocument();
   });
 });
